9a37b47177720a08eed67d5bb8521a8ee3b3e439
kalaiselvankadirvel <kalaiselvan@outlook.com>
2019-07-06 21:17:48 -0400

    Added Add,Update and Delete with dummy objects

--- Todo/src/main/java/in/kalaiselvan/todo/webservice/TodoApplication.java
+++ Todo/src/main/java/in/kalaiselvan/todo/webservice/TodoApplication.java
@@ -2,12 +2,13 @@ package in.kalaiselvan.todo.webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "in.kalaiselvan.todo" )
 public class TodoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
 	}
-
 }
